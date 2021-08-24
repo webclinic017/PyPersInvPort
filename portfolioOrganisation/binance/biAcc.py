3ec59cb35efb4b9859f0d6bbf71597f506fd3a86
noctemn <ferteinnoctem@protonmail.com>
2021-08-25 00:36:03 +0800

    deposit and withdrawal history

--- portfolioOrganisation/binance/biAcc.py
+++ portfolioOrganisation/binance/biAcc.py
@@ -1,4 +1,4 @@
-from binance.spot.wallet import withdraw
+from binance.spot.wallet import deposit_history, withdraw
 from binance.spot import Spot as Client
 from binance.lib.utils import config_logging
 from datetime import datetime
@@ -20,7 +20,7 @@ client = Client(key, secret)
 
 
 df=pd.DataFrame(client.account()["balances"])
-print(df)
+# print(df)
 # print(df['free'>0])
 
 df['free']=pd.to_numeric(df['free'])
@@ -29,8 +29,79 @@ df['locked']=pd.to_numeric(df['locked'])
 # print(df['free']>0)
 ndf = df[df['free']>0]
 print(ndf)
+symbols = [x for x in ndf['asset']]
+
+all_combos = [x+y for x in symbols for y in symbols]
+print(type(all_combos))
 
 
-# df = pd.DataFrame(client.account)
-# print(df)
+
+
+
+pairs = [x['symbol'] for x in client.exchange_info()['symbols']]
+
+
+# print(pairs)
+
+set_of_pairs = set(pairs)
+set_of_all_combos = set(all_combos)
+set_of_pairs_possibly_traded = set_of_pairs & set_of_all_combos
+
+# print(type(set_of_pairs))
+print(set_of_pairs_possibly_traded)
+
+
+
+
+deposit_history_df = pd.DataFrame(client.deposit_history())
+
+# pp.pprint(client.withdraw_history())
+withdraw_history_df = pd.DataFrame(client.withdraw_history())
+
+
+def myTrades(listOfSymbols = list, readableTime = False, csv = False):
+    Trades = []
+    
+    def niceDates(binanceDate):
+        return str(datetime.fromtimestamp(int(binanceDate/1000)))
+
+    for x in listOfSymbols:
+        Trades = Trades + client.my_trades(x)
+        df = pd.DataFrame(Trades)
+        arr = np.array(Trades)
+    if readableTime == True: 
+        df['time'] = df['time'].apply(niceDates)
+    
+    cwd = os.getcwd()
+    path = cwd + "/{}.csv".format("biAcc")
+    if csv == True: 
+        return df.to_csv(path)
+    else: 
+        return df
+
+
+myTrades(set_of_pairs_possibly_traded)
+print(myTrades(set_of_pairs_possibly_traded))
+
+print(deposit_history_df)
+
 # %%
+
+
+# %%
+
+
+
+# trades = []
+# for x in symbols: 
+#     trades.append
+# trade_history_df = pd.DataFrame(client.my_trades(symbols))
+# print(trade_history_df)
+# # pp.pprint(client.account())
+
+# # pp.pprint(client.funding_wallet())
+
+
+# # df = pd.DataFrame(client.account)
+# # print(df)
+# # %%
